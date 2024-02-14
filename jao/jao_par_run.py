eab1bf06a048420d6149890fea5c5d73115cd436
Frank Boerman <frank@fboerman.nl>
2024-02-14 22:38:57 +0100

    added ntc and atc query to intraday client

--- jao/jao_par_run.py
+++ jao/jao_par_run.py
@@ -1,5 +1,7 @@
 from .jao import JaoPublicationToolPandasClient
 import pandas as pd
+from typing import List, Dict
+from .parsers import parse_base_output
 
 
 class JaoPublicationToolPandasNordics(JaoPublicationToolPandasClient):
@@ -34,3 +36,35 @@ class JaoPublicationToolPandasIntraDay(JaoPublicationToolPandasClient):
 
     def query_active_constraints(self, day: pd.Timestamp):
         raise NotImplementedError
+
+    def query_sidc_atc_raw(self, day: pd.Timestamp) -> List[Dict]:
+        return self._query_base_day(day, 'intradayAtc')
+
+    def query_sidc_ntc_raw(self, day: pd.Timestamp) -> List[Dict]:
+        return self._query_base_day(day, 'intradayNtc')
+
+    def query_sidc_atc(self, day: pd.Timestamp, from_zone: str = None, to_zone: str = None) -> pd.DataFrame:
+        df = parse_base_output(
+            self.query_sidc_atc_raw(day=day)
+        ).rename(columns=lambda x: x.lstrip('border_').replace('_', '>'))
+
+        if from_zone is not None:
+            df = df[[c for c in df.columns if c.split('>')[0] == from_zone]]
+
+        if to_zone is not None:
+            df = df[[c for c in df.columns if c.split('>')[1] == to_zone]]
+
+        return df
+
+    def query_sidc_ntc(self, day: pd.Timestamp, from_zone: str = None, to_zone: str = None) -> pd.DataFrame:
+        df = parse_base_output(
+            self.query_sidc_ntc_raw(day=day)
+        ).rename(columns=lambda x: x.lstrip('border_').replace('_', '>'))
+
+        if from_zone is not None:
+            df = df[[c for c in df.columns if c.split('>')[0] == from_zone]]
+
+        if to_zone is not None:
+            df = df[[c for c in df.columns if c.split('>')[1] == to_zone]]
+
+        return df
